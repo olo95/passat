8ed7251a00e845593f5fe1761e0457f63a59abe1
Alex S <alexander.stolar@yahoo.com>
2020-08-31 21:42:40 +0200

    removed sample code, brief cleaning, added needed packages

---
+++ lib/app/passat_app.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class PassatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp();
+}
